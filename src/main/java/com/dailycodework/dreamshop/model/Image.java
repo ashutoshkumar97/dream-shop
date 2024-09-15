e2fbe638bd1f4f18f2541aa2767ea0c9ee5d43e3
ashutoshdhiraniya <ashutosh971880@gmail.com>
2024-09-16 01:09:44 +0530

    Added Entity classes : Category, Image and Product

---
+++ src/main/java/com/dailycodework/dreamshop/model/Image.java
@@ -0,0 +1,31 @@
+package com.dailycodework.dreamshop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Blob;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String imageName;
+    private String imageType;
+    private String imageUrl;
+
+    @Lob
+    private Blob image;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+}
---
+++ src/main/java/com/dailycodework/dreamshop/model/Product.java
@@ -0,0 +1,34 @@
+package com.dailycodework.dreamshop.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private BigDecimal price;
+    private int inventory;
+    private String description;
+
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Image> images;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "category")
+    private Category category;
+}
